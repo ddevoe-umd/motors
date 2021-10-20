7075d5f1feab421ccd08ff911ae2dc4488cae8a7
ddevoe-umd <84353435+ddevoe-umd@users.noreply.github.com>
2021-10-19 20:05:12 -0400

    Update servo.py

--- servo.py
+++ servo.py
@@ -3,23 +3,27 @@
 # 1 ms ON = full CW = 5% duty cycle
 # 2 ms ON = full CCW = 10% duty cycle
 
-import RPi.GPIO as gpio
+import RPi.GPIO as GPIO
 import time
 
-gpio.setmode(gpio.BCM)
+GPIO.setmode(GPIO.BCM)
 pwmPin = 24
-gpio.setup(pwmPin, gpio.OUT)
+GPIO.setup(pwmPin, GPIO.OUT)
 
 # set min & max % duty cycles (5 and 10 are default values, but play
 # around to find optimum values for your motor)
 dcMin = 3
 dcMax = 12
 
-pwm = gpio.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
+pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
 pwm.start(0)
 
-While True:
-  for dc in range(dcMin,dcMax):
-    pwm.ChangeDutyCycle(dc)
-    print dc
-    time.sleep(0.5)
+try:
+  while True:
+    for dc in range(dcMin,dcMax):
+      pwm.ChangeDutyCycle(dc)
+      print(dc)
+      time.sleep(0.5)
+except KeyboardInterrupt:
+  print("closing")
+GPIO.cleanup()
